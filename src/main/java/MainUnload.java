219ae6644e8ef0ac2599e2fabdaa1c123afe36dc
Sergei <sergei.visotsky@gmail.com>
2018-03-03 17:09:54 +0200

    swing window added

--- src/main/java/MainUnload.java
+++ src/main/java/MainUnload.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class MainUnload {
     public static void main(String[] args) {
-
+        JFrame window = new JFrame();
+        window.setTitle("Symbol strings");
+        JPanel panel = new JPanel();
+        window.add(panel);
+        window.setVisible(true);
+        window.setSize(300, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
