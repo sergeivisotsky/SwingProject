24fabe75a9ac9fe01ade410cfe810f0648868aaf
Sergei <sergei.visotsky@gmail.com>
2018-03-18 08:56:03 +0200

    Window size bug fix

--- src/main/java/MainUnload.java
+++ src/main/java/MainUnload.java
@@ -4,11 +4,6 @@ public class MainUnload {
     public static void main(String[] args) {
         PrepareGUI window = new PrepareGUI();
         window.setTitle("String convertion");
-        Dimension screenSize =
-                Toolkit.getDefaultToolkit().getScreenSize();
-        int height = screenSize.height;
-        int width = screenSize.width;
-        window.setSize(width / 2, height / 2);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
