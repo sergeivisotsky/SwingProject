3d787da2b826c7adeaf90164f8dde10116c82153
Sergei <sergei.visotsky@gmail.com>
2018-03-18 08:01:48 +0200

    Show window on the center of screen

--- src/main/java/MainUnload.java
+++ src/main/java/MainUnload.java
@@ -1,8 +1,15 @@
+import java.awt.*;
+
 public class MainUnload {
     public static void main(String[] args) {
-        PrepareGUI prepareGUI = new PrepareGUI();
-        prepareGUI.setTitle("String convertion");
-        prepareGUI.setLocationRelativeTo(null);
-        prepareGUI.setVisible(true);
+        PrepareGUI window = new PrepareGUI();
+        window.setTitle("String convertion");
+        Dimension screenSize =
+                Toolkit.getDefaultToolkit().getScreenSize();
+        int height = screenSize.height;
+        int width = screenSize.width;
+        window.setSize(width / 2, height / 2);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
